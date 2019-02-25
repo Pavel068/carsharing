9aa334129cfdcea07111011f1536865f9b3088dd
Pavel Trefilov <9889@inbox.ru>
2019-02-25 10:52:08 +0300

    Login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    active: '/'
+  });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    active: '/login'
+  });
 });
 
 module.exports = router;
