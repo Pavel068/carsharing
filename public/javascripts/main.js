b82304f7a5ff173aa42bf8c893e52176c12180fb
Pavel Trefilov <9889@inbox.ru>
2019-02-26 13:06:50 +0300

    Index fix

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,15 @@
-var socket = io();
-
-ymaps.ready(init);
-
 function init() {
     var myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
         zoom: 7
     });
 }
+
+$(document).ready(() => {
+    var socket = io();
+
+    if ($("#map").length > 0) {
+        ymaps.ready(init);
+    }
+
+});
