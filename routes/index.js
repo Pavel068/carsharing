4113fc09a6b6f72e0f0f710858e44bf605d1a982
Pavel Trefilov <9889@inbox.ru>
2019-02-27 11:33:22 +0300

    Register

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const db = new (require('../lib/db'))();
+const user = new (require('../lib/user'))(db);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    active: '/',
-    breadcrumbs: [
-      {
-        'label': 'Главная',
-        'href': '/'
-      },
-    ],
-  });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        active: '/',
+        breadcrumbs: [
+            {
+                'label': 'Главная',
+                'href': '/'
+            },
+        ],
+    });
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login', {
-    active: '/login',
-    breadcrumbs: [
-      {
-        'label': 'Авторизация',
-        'href': '/login'
-      },
-    ],
-  });
+router.get('/login', function (req, res, next) {
+    res.render('login', {
+        active: '/login',
+        breadcrumbs: [
+            {
+                'label': 'Авторизация',
+                'href': '/login'
+            },
+        ],
+    });
 });
 
 /* GET register page. */
-router.get('/register', function(req, res, next) {
-  res.render('register', {
-    active: '/register',
-    breadcrumbs: [
-      {
-        'label': 'Регистрация',
-        'href': '/register'
-      },
-    ],
-  });
+router.get('/register', function (req, res, next) {
+    res.render('register', {
+        active: '/register',
+        breadcrumbs: [
+            {
+                'label': 'Регистрация',
+                'href': '/register'
+            },
+        ],
+    });
+});
+
+/* Add user */
+router.post('/register', (req, res, next) => {
+    user.register(req.body);
 });
 
 module.exports = router;
