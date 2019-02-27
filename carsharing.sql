6e418fe09f4ad46f29ac2d6a26203a965587496a
Pavel Trefilov <9889@inbox.ru>
2019-02-27 14:38:31 +0300

    DB fix

--- carsharing.sql
+++ carsharing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 27 2019 г., 13:47
+-- Время создания: Фев 27 2019 г., 14:38
 -- Версия сервера: 5.6.41
 -- Версия PHP: 7.2.10
 
@@ -119,7 +119,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `name`, `surname`, `patr`, `birthday`, `avatar`, `is_admin`) VALUES
-(1, 'admin', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', 'Admin', '', '', '0000-00-00', '', 0);
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Admin', 'Ad', NULL, NULL, NULL, 1),
+(2, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'Test', 'T', NULL, NULL, NULL, 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -184,7 +185,7 @@ ALTER TABLE `trips`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
