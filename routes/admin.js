93a6bb9f5488925f0dfa0d714d66180d6a6268f1
Pavel Trefilov <9889@inbox.ru>
2019-03-01 11:13:24 +0300

    Add car function

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const db = new (require('../lib/db'))();
+const car = new (require('../lib/car'))(db);
+
 /* GET home admin page. */
 router.get('/', function (req, res, next) {
     if (user.info.id === undefined || user.info.is_admin !== 1) {
@@ -21,7 +24,14 @@ router.get('/', function (req, res, next) {
 
 /* Add new car */
 router.post('/cars', (req, res, next) => {
-
+    car.addCar(req.body)
+        .then((response) => {
+            res.redirect('/admin');
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send('Error');
+        });
 });
 
 module.exports = router;
